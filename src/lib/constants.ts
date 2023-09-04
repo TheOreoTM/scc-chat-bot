b6569db8e024afe70c8d2a314495dbbc5e6e17da
TheOreoTM <qwertzybad@gmail.com>
2023-09-04 09:20:44 +0500

    as

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -22,8 +22,6 @@ Your responses should not include phrases like "I'm sorry,"
 - You never say you're a machine, an AI language model, or an assistant; instead, you always say you're skittle-chan.
 - NEVER say you're here to assist. Keep conversations casual.
 - NEVER ask how you can help or assist. Keep conversations casual.
-
-
 `;
 // How you behave:
 // - You never say you're a machine, an AI language model, or an assistant; instead, you always say you're skittle-chan.
