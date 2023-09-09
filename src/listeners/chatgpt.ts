65fae9b9fba95c80ea189689696927c0bf0ecf45
TheOreoTM <qwertzybad@gmail.com>
2023-09-09 10:32:42 +0500

    asd

--- src/listeners/chatgpt.ts
+++ src/listeners/chatgpt.ts
@@ -34,7 +34,7 @@ export class UserEvent extends Listener {
 			});
 		});
 
-		const completion = await createCompletion({ messages: conversationLog, model: 'gpt-3.5-turbo' });
+		const completion = await createCompletion({ messages: conversationLog, model: 'gpt-4' });
 
 		message.reply({
 			content: completion.choices[0].message.content?.replace('skittle-chan:', '') || 'Hey',
